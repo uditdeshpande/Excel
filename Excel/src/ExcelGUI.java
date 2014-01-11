66364dc06dffa4a4b0909b1d6af817015b0c392a
uditdeshpande <deshpande.udit@gmail.com>
2014-01-11 10:33:16 +0530

    GUI has been completed

--- Excel/src/ExcelGUI.java
+++ Excel/src/ExcelGUI.java
@@ -1,10 +1,15 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -13,22 +18,31 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.TableColumn;
 
 
 public class ExcelGUI extends JFrame implements ActionListener{
 
-	JPanel root,north,west;
-	JButton browse,accept,generate;
+	JPanel root,north,east,south,tabr,imgr,tabt,tabb;
+	JButton browse,accept,generate,submit,invaxis,about,help,save_chart,clear,addrow,addcol;
 	JTextField filein;
 	JLabel file,status;
 	JComboBox graphtype;
+	JTable table;
 	String[] graph=new String[] {"Stack Chart","Pie Chart","Bar Chart","Line Chart"};
+	JScrollPane pane;
+	
+	
 	public ExcelGUI()
 	{
+		this.setLayout(new BorderLayout());
 	setupUI();
 	addListener();
-	this.setLayout(new FlowLayout(FlowLayout.LEFT));
+	
 	this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	this.setVisible(true);	
@@ -42,8 +56,26 @@ public class ExcelGUI extends JFrame implements ActionListener{
 	
 	void setupUI()
 	{
+		
 		north=new JPanel(new FlowLayout(FlowLayout.LEFT));
-		this.add(north);
+		root=new JPanel(new FlowLayout(FlowLayout.LEFT));
+		tabr=new JPanel();imgr=new JPanel();
+		tabr.setLayout(new BoxLayout(tabr,BoxLayout.Y_AXIS));
+		tabt=new JPanel(new FlowLayout());tabb=new JPanel(new FlowLayout());
+		tabr.add(tabt);tabr.add(tabb);
+		root.add(tabr);root.add(imgr);
+		south=new JPanel(new FlowLayout(FlowLayout.CENTER));
+		east=new JPanel();
+		east.setLayout(new BoxLayout(east,BoxLayout.Y_AXIS));
+		
+		
+		this.add(north, BorderLayout.NORTH);
+		this.add(root,BorderLayout.CENTER);
+		this.add(east,BorderLayout.EAST);
+		this.add(south,BorderLayout.SOUTH);
+		//NORTH PANEL UI COMPONENTS
+		
+		//this.add(north);
 		filein=new JTextField();
 		filein.setColumns(50);
 		file=new JLabel("Please choose the excel file");
@@ -57,6 +89,35 @@ public class ExcelGUI extends JFrame implements ActionListener{
 		north.add(graphtype);
 		north.add(accept);
 		north.add(status);
+		
+		
+		//WEST COMPONENTS ADD
+		invaxis=new JButton("  Invert Axis   ");
+		about=new JButton("    About Us    ");
+		help=new JButton("         Help        ");
+		save_chart=new JButton("  Save Chart  ");
+		clear=new JButton("Clear Screen");
+		east.add(invaxis);
+		east.add(about);
+		east.add(help);
+		east.add(save_chart);
+		east.add(clear);
+		//SOUTH PANEL UI COMPONENTS
+//		south.setBackground(Color.BLACK);
+//		submit=new JButton("Submit");
+//		south.add(submit);
+//		
+		String[] head={"Details","Attribute A","Attribute B"};
+		Object[][] data= {{"A",1,2},{"B",3,4},{"C",5,6}};
+		table=new JTable(data,head);
+		
+		pane=new JScrollPane(table);
+		
+		table.setFillsViewportHeight(true);
+		tabt.add(pane);
+		addrow=new JButton("Add ROW");
+		addcol=new JButton("Add COL");
+		tabb.add(addrow);tabb.add(addcol);
 		
 	}
 
@@ -79,8 +140,8 @@ public class ExcelGUI extends JFrame implements ActionListener{
 			{
 				status.setForeground(Color.RED);
 				status.setText("File read failed!");
-				
 			}	
+			table.addColumn(new TableColumn());
 			
 		
 		}
